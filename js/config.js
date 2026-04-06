c70b58a869364b4b664f3fbaeef2854389c41d67
disku0001 <54368079+disku0001@users.noreply.github.com>
2026-04-06 06:12:23 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ const CONFIG = {
   offerUrl: 'https://example.com/offer',
 
   // UTM params to capture and forward
-  utmParams: ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'],
+  //utmParams: ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'],
 
   // SubID key for affiliate network
-  affiliateSubIdKey: 'subid'
+  //affiliateSubIdKey: 'subid'
 };
