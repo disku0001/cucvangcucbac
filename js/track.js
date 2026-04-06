81feacc4e25734b333aa044ae204ef219a562743
disku0001 <disku0001@users.noreply.github.com>
2026-04-06 02:02:52 -0500

    Rolling digit animation for social proof counter

--- js/track.js
+++ js/track.js
@@ -20,12 +20,48 @@
     location.href=url;
   });
 
-  // Social proof counter
+  // Rolling counter
   var el=document.getElementById('claimCount');
   if(!el)return;
   var n=23847;
+
+  function render(num){
+    var s=num.toLocaleString();
+    var html='';
+    for(var i=0;i<s.length;i++){
+      var c=s[i];
+      if(c>='0'&&c<='9'){
+        var d=parseInt(c);
+        html+='<span class="roller__digit"><span class="roller__strip" data-d="'+d+'" style="transform:translateY(-'+d+'0%)">';
+        for(var j=0;j<=9;j++)html+='<span>'+j+'</span>';
+        html+='</span></span>';
+      }else{
+        html+=c;
+      }
+    }
+    el.innerHTML=html;
+  }
+
+  function update(num){
+    var s=num.toLocaleString();
+    var digits=el.querySelectorAll('.roller__strip');
+    var di=0;
+    for(var i=0;i<s.length;i++){
+      var c=s[i];
+      if(c>='0'&&c<='9'){
+        if(digits[di]){
+          digits[di].style.transform='translateY(-'+c+'0%)';
+          digits[di].setAttribute('data-d',c);
+        }
+        di++;
+      }
+    }
+  }
+
+  render(n);
+
   setInterval(function(){
     n+=Math.floor(Math.random()*3)+1;
-    el.textContent=n.toLocaleString();
+    update(n);
   },Math.floor(Math.random()*6000)+8000);
 }();
