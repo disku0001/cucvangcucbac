5fc9c52546cebd356d10010baf09c1b4b544718a
disku0001 <54368079+disku0001@users.noreply.github.com>
2026-06-02 11:52:59 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ const CONFIG = {
   offerUrl: 'https://giftclick.org/aff_c?offer_id=1113&aff_id=175473',
 
   // UTM params to capture and forward
-  //utmParams: ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'],
+  // utmParams: ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'],
 
   // SubID key for affiliate network
-  //affiliateSubIdKey: 'subid'
+  // affiliateSubIdKey: 'subid'
 };
