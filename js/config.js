290fd4a9153324079da555d052669cf2523ea0b9
disku0001 <54368079+disku0001@users.noreply.github.com>
2026-06-02 12:12:46 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   // Offer — your real affiliate URL
-  offerUrl: 'https://giftclick.org/aff_c?offer_id=1113&aff_id=175473'
+  offerUrl: 'https://trksy.org/aff_c?offer_id=365&aff_id=175473'
 };
